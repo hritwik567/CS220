ecf45190e15599c40b0eea45772806086eaeae36
Anay Mehrotra <mehrotranay@gmail.com>
2018-02-04 16:54:06 +0530

    Test comit

--- Lab1/Lab1_3/two_bit_adder/two_bit_adder.v
+++ Lab1/Lab1_3/two_bit_adder/two_bit_adder.v
@@ -19,10 +19,12 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module two_bit_adder(a,b,cout,sum);
-input [1:0]a,[1:0]b;
-output [1:0]sum, cout;
+input [1:0]a,b;
+output [1:0]sum;
+output cout;
 
-wire [1:0]sum, cout;
+wire [1:0]sum;
+wire cout;
 wire carry0;
 
 full_adder uut1 (a[0],b[0],1'b0,sum[0],carry0);
