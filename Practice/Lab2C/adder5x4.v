ec56d3d144c6cc7b84dd937d7d65c13d3e45ea19
Anay Mehrotra <mehrotranay@gmail.com>
2018-02-04 19:09:48 +0530

    add lab2c

---
+++ Practice/Lab2C/adder5x4.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date:    14:34:51 01/29/2018
+// Design Name:
+// Module Name:    seven_bit_adder
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+module adder5x4(y,pb1,pb2,pb3,pb4,rot,sum);
+input [3:0]y;
+input pb1,pb2,pb3,pb4;
+
+output [7:0]sum;
+wire [6:0]sum;
+
+reg [3:0]a,b,c,d;
+reg [5:0]x,y;
+reg [6:0]z,e;
+
+always @(posedge pb1) begin
+  a<=y;
+end
+
+always @(posedge pb2) begin
+  b<=y;
+end
+
+always @(posedge pb3) begin
+  c<=y;
+end
+
+always @(posedge pb4) begin
+  d<=y;
+end
+
+always @(posedge rot) begin
+  e[6:4]<=3'b000;
+  e[3:0]<=y;
+end
+
+adder4 fa41 (a,b,x);
+adder4 fa42 (c,d,y);
+adder5 fa5  (x,y,z);
+adder6 fa6  (z,e,sum);
+
+endmodule
--- Practice/template.v
+++ Practice/template.v
@@ -1,11 +1,68 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:34:51 01/29/2018 
+// Design Name: 
+// Module Name:    seven_bit_adder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module seven_bit_adder(y,pb1,pb2,pb3,pb4,sum,cout);
+input [3:0]y;
+input pb1;
+input pb2;
+input pb3;
+input pb4;
 
-module fulladder(a,b,c,sum,carry);
-input a, b, c;
-output sum, carry;
+output [6:0]sum;
+output cout;
 
-wire sum, carry;
+reg [6:0]a;
+reg [6:0]b;
 
-assign sum = a^b^c;
-assign carry = ((a&b)|(b&c)|(c&a));
+wire [6:0]sum;
+wire cout; 
 
-endmodule 
+wire carry0;
+wire carry1;
+wire carry2;
+wire carry3;
+wire carry4;
+wire carry5;
+
+always @(posedge pb1) begin
+  a[3:0] <= y;
+end
+
+always @(posedge pb2) begin
+  a[6:4] <= y[2:0];
+end
+
+always @(posedge pb3) begin
+  b[3:0] <= y;
+end
+
+always @(posedge pb4) begin
+  b[6:4] <= y[2:0];
+end
+
+full_adder fa1 (a[0],b[0],1'b0,sum[0],carry0);
+full_adder fa2 (a[1],b[1],carry0,sum[1],carry1);
+full_adder fa3 (a[2],b[2],carry1,sum[2],carry2);
+full_adder fa4 (a[3],b[3],carry2,sum[3],carry3);
+full_adder fa5 (a[4],b[4],carry3,sum[4],carry4);
+full_adder fa6 (a[5],b[5],carry4,sum[5],carry5);
+full_adder fa7 (a[6],b[6],carry5,sum[6],cout);
+
+endmodule
