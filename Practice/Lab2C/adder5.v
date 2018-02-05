89fee42ee83db8c9ee431dc3cc590640145c881a
Hritvik Taneja <hritwik567@gmail.com>
2018-02-05 10:30:28 +0530

    Change Lab2C

--- Practice/Lab2C/adder5.v
+++ Practice/Lab2C/adder5.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-`include "full_adder.v"
+`include "adder4.v"
 //////////////////////////////////////////////////////////////////////////////////
 // Company:
 // Engineer:
@@ -28,8 +28,8 @@ module adder5(a,b,sum);
 
 	full_adder uut1 (a[0],b[0],1'b0,sum[0],carry[0]);
 	full_adder uut2 (a[1],b[1],carry[0],sum[1],carry[1]);
-  full_adder uut3 (a[2],b[2],carry[1],sum[2],carry[2]);
-  full_adder uut4 (a[3],b[3],carry[2],sum[3],carry[3]);
+	full_adder uut3 (a[2],b[2],carry[1],sum[2],carry[2]);
+  	full_adder uut4 (a[3],b[3],carry[2],sum[3],carry[3]);
 	full_adder uut5 (a[4],b[4],carry[3],sum[4],sum[5]);
 
 endmodule
