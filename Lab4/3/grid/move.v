217eae2ea6b5fd4b8d1815914bbbb4d7424ca443
Anay Mehrotra <mehrotranay@gmail.com>
2018-03-06 21:27:38 +0530

    add sequential logic in lab5/3/grid/move.v

--- Lab4/3/grid/move.v
+++ Lab4/3/grid/move.v
@@ -31,19 +31,19 @@ wire [3:0]tmp;
 wire last;
 
 adder5 add5 (pos,step,op,tmp,last);
-assign out[0] = (tmp[0]&(~last))|((~op)&last);
-assign out[1] = (tmp[1]&(~last))|((~op)&last);
-assign out[2] = (tmp[2]&(~last))|((~op)&last);
-assign out[3] = (tmp[3]&(~last))|((~op)&last);
+// assign out[0] = (tmp[0]&(~last))|((~op)&last);
+// assign out[1] = (tmp[1]&(~last))|((~op)&last);
+// assign out[2] = (tmp[2]&(~last))|((~op)&last);
+// assign out[3] = (tmp[3]&(~last))|((~op)&last);
 
-// always@(posedge clk) begin
-// 	if(op==0 && last==1)begin
-// 		out<=4'b1111;
-// 	end else if(op==1 && last==0) begin
-// 		out<=4'b0;
-// 	end else begin
-// 		out<=tmp;
-// 	end
-// end
+always@(posedge clk) begin
+	if(op==0&&last==1)begin
+		out<=4'b1111;
+	end else if(op==1&&last==0) begin
+		out<=4'b0;
+	end else begin
+		out<=tmp;
+	end
+end
 
 endmodule
