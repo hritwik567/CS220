d2cd4cb8d34eb855e11a5cfc212b37a9e896a354
Anay Mehrotra <mehrotranay@gmail.com>
2018-03-18 11:39:25 +0530

    add lab6/ complete switch-case for op

--- Practice/lab6/controller.v
+++ Practice/lab6/controller.v
@@ -1,62 +1,161 @@
 module(
 	input [3:0] in,
 	input clk,
+	input pb,//pushbutton
 	input rot_a,
 	input rot_b,
 	);
 
-	reg rot_e, p_rot_e;
-	reg op;
-	reg [2:0] counter = 3'b0;
+	reg rot_e, prev;
+	reg [2:0] op;
+	reg [2:0] cnt = 3'b0;
+
+	reg [3:0] shift;
 
 	reg readA, readB, write;
 	reg [4:0] wrAddr, rdAddrA, rdAddrB;
 	reg [15:0] wrData, rdDataA, rdDataB;
 
 	always@(posedge clk) begin
+
 		if(rot_a & rot_b) begin
 			rot_e <= 1;
-		end
-		else if(!rot_a & !rot_b) begin
+		end else if(!rot_a & !rot_b) begin
 			rot_e <= 0;
 		end
-		p_rot_e <= rot_e;
-		if(!p_rot_e & rot_e) begin
-			if(counter==0) begin
-				op <= in[2:0];
-			end
-			else if(counter==1) begin
-				case(op):
-					3'b0: wrAddr[3:0] <= in;
-				endcase
-			end
-			else if(counter==2) begin
-				case(op):
-					3'b0: wrAddr[4] <= in[0];
-				endcase
-			end
-			else if(counter==3) begin
-				case(op):
-					3'b0: wrData[3:0] <= in;
-				endcase
-			end
-			else if(counter==4) begin
-				case(op):
-					3'b0: wrData[7:4] <= in;
-				endcase
-			end
-			else if(counter==5) begin
-				case(op):
-					3'b0: wrData[11:8] <= in;
-				endcase
-			end
-			else if(counter==6) begin
-				case(op):
-					3'b0: wrData[15:12] <= in;
-				endcase
-			end
 
-			counter <= counter + 1;
+		if(pb) begin
+			cnt=0;
+			//reset
+		end
+
+		prev <= rot_e;
+
+		if(!prev & rot_e) begin
+			op <= in[2:0];
+			if(cnt==0) begin
+				op <= in[2:0];
+			end else if(cnt==1) begin
+				case(op):
+					3'b000:wrAddr[3:0] <= in;
+					default:rdAddrA[3:0] <= in;
+				endcase
+			end else if(cnt==2) begin
+				case(op):
+					3'b000:wrAddr[4] <= in[0];
+					default:rdAddrA[4] <= in[0];
+				endcase
+			end else if(cnt==3) begin
+				case(op):
+					3'b000:wrData[3:0] <= in;
+					3'b001://dosomething cnt<=10;
+					3'b010:rdAddrB[3:0] <= in;
+					3'b011:wrAddr[3:0] <= in;
+					3'b100:rdAddrB[3:0] <= in;
+					3'b101:rdAddrB[3:0] <= in;
+					3'b110:rdAddrB[3:0] <= in;
+					3'b111:shift<=in;
+				endcase
+			end else if(cnt==4) begin
+				case(op):
+					3'b000:wrData[7:4] <= in;
+					3'b001:
+					3'b010:rdAddrB[4] <= in[0];
+					3'b011:wrAddr[4] <= in[0];
+					3'b100:rdAddrB[4] <= in[0];
+					3'b101:rdAddrB[4] <= in[0];
+					3'b110:rdAddrB[4] <= in[0];
+					3'b111:wrAddr[3:0] <= in;
+				endcase
+			end else if(cnt==5) begin
+				case(op):
+					3'b000:wrData[11:8] <= in;
+					3'b001:
+					3'b010://dosomething cnt<=10;
+					3'b011:wrData[3:0] <= in;
+					3'b100:wrAddr[3:0] <= in;
+					3'b101:
+					3'b110:
+					3'b111:wrAddr[4] <= in[0];
+				endcase
+			end else if(cnt==6) begin
+				case(op):
+					3'b000:wrData[15:12] <= in;
+					3'b001:
+					3'b010:
+					3'b011:wrData[7:4] <= in;
+					3'b100:wrAddr[4] <= in[0];
+					3'b101:wrAddr[4] <= in[0];
+					3'b110:wrAddr[4] <= in[0];
+					3'b111://dosomethingcnt<=10
+				endcase
+			end
+			end else if(cnt==7) begin
+				case(op):
+					3'b000://dosomething cnt<=10;
+					3'b001:
+					3'b010:
+					3'b011:wrData[11:8] <= in;
+					3'b100:wrData[3:0] <= in;
+					3'b101://dosomethingcnt<=10;
+					3'b110://dosomethingcnt<=10;
+					3'b111:
+				endcase
+			end
+			end else if(cnt==8) begin
+				case(op):
+					3'b000:
+					3'b001:
+					3'b010:
+					3'b011:wrData[15:12] <= in;
+					3'b100:wrData[7:4] <= in;
+					3'b101:
+					3'b110:
+					3'b111:
+				endcase
+			end
+			end else if(cnt==9) begin
+				case(op):
+					3'b000:
+					3'b001:
+					3'b010:
+					3'b011://dosomethingcnt<=10;
+					3'b100:wrData[11:8] <= in;
+					3'b101:
+					3'b110:
+					3'b111:
+				endcase
+			end
+			end else if(cnt==10) begin
+				case(op):
+					3'b000:
+					3'b001:
+					3'b010:
+					3'b011:
+					3'b100:wrData[15:12] <= in;
+					3'b101:
+					3'b110:
+					3'b111:
+				endcase
+			end
+			end else if(cnt==10) begin
+				case(op):
+					3'b000:
+					3'b001:
+					3'b010:
+					3'b011:
+					3'b100://dosomethingcnt<=10
+					3'b101:
+					3'b110:
+					3'b111:
+				endcase
+			end
+			cnt <= cnt + 1;
+
+			if(cnt==11) begin
+				#5cnt<=0;
+				//reset, cnt, opcode, addresses, shift, value
+			end
 		end
 
 endmodule
--- Practice/lab6/regfile.v
+++ Practice/lab6/regfile.v
@@ -11,18 +11,28 @@ module regfile(
 	output reg [15:0] rdDataB
 	);
 
-	reg [15:0] regfile [0:31];
-//TODO: initialize all the registers to zero
-	always @(posedge clk) begin
-		if(readA) rdDataA <= regfile[rdAddrA];
+	//regfile renamed to file, as it clashed with module name
+	reg [15:0] file [0:31];
+	initial begin
+		file[0]=16'b0;file[1]=16'b0;file[2]=16'b0;file[3]=16'b0;file[4]=16'b0;
+		file[5]=16'b0;file[6]=16'b0;file[7]=16'b0;file[8]=16'b0;file[9]=16'b0;
+		file[10]=16'b0;file[11]=16'b0;file[12]=16'b0;file[13]=16'b0;file[14]=16'b0;
+		file[15]=16'b0;file[16]=16'b0;file[17]=16'b0;file[18]=16'b0;file[19]=16'b0;
+		file[20]=16'b0;file[21]=16'b0;file[22]=16'b0;file[23]=16'b0;file[24]=16'b0;
+		file[25]=16'b0;file[26]=16'b0;file[27]=16'b0;file[28]=16'b0;file[29]=16'b0;
+		file[30]=16'b0;file[31]=16'b0;
 	end
 
 	always @(posedge clk) begin
-		if(readB) rdDataB <= regfile[rdAddrB];
+		if(readA) rdDataA <= file[rdAddrA];
 	end
 
 	always @(posedge clk) begin
-		if (write) regfile[wrAddr] <= wrData;
+		if(readB) rdDataB <= file[rdAddrB];
+	end
+
+	always @(posedge clk) begin
+		if (write) file[wrAddr] <= wrData;
 	end
 
 endmodule
